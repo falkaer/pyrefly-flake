edc9c1abc02cccbd2a54d820d0d4a7a8ab4fe2be
falkaer <kenny@falkaer.io>
2025-10-23 12:32:20 +0200

    init

---
+++ flake.nix
@@ -0,0 +1,61 @@
+# based on:
+# https://github.com/NixOS/nixpkgs/blob/nixos-unstable/pkgs/by-name/py/pyrefly/package.nix
+{
+  description = "Pyrefly - A fast type checker and IDE for Python";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        packages = {
+          pyrefly = pkgs.rustPlatform.buildRustPackage (finalAttrs: {
+            pname = "pyrefly";
+            version = "0.38.0";
+
+            src = pkgs.fetchFromGitHub {
+              owner = "facebook";
+              repo = "pyrefly";
+              tag = finalAttrs.version;
+              hash = "sha256-HPPDsvWEFfh/GNMUPiVjQr28YBBs2DACBGM3cxo5Nx4=";
+            };
+
+            buildAndTestSubdir = "pyrefly";
+            cargoHash = "sha256-46kcoBG/PWwf8VdlvLNzEhfYRTmmKi/uTjwFkl7Wozg=";
+
+            nativeInstallCheckInputs = [ pkgs.versionCheckHook ];
+            doInstallCheck = true;
+
+            # Requires unstable rust features
+            env.RUSTC_BOOTSTRAP = 1;
+
+            meta = with pkgs.lib; {
+              description = "Fast type checker and IDE for Python";
+              homepage = "https://github.com/facebook/pyrefly";
+              license = licenses.mit;
+              mainProgram = "pyrefly";
+              platforms = platforms.linux ++ platforms.darwin;
+              maintainers = [ ];
+            };
+          });
+          default = self.packages.${system}.pyrefly;
+        };
+        apps = {
+          pyrefly = flake-utils.lib.mkApp {
+            drv = self.packages.${system}.pyrefly;
+          };
+          default = self.apps.${system}.pyrefly;
+        };
+      }
+    );
+}
